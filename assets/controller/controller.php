16d201de7b7bca968d674a4679d046c2c1264305
Breesho <felix.noel3@orange.fr>
2020-06-30 16:18:32 +0200

    echo

--- assets/controller/controller.php
+++ assets/controller/controller.php
@@ -1,4 +1,11 @@
 <?php
+    $fichier = 'source.xml';
+    $contenu = simplexml_load_file($fichier);
+    $numpage='';
 
+    if(isset($_GET['page'])) {
+        $numpage = $_GET['page'];
+    }
 
+        
 ?>
--- index.php
+++ index.php
@@ -1,21 +1,34 @@
 <?php
-
-require_once 'assets/controller/controller.php'
-
-?> 
+    require_once 'assets/controller/controller.php';
+?>
 
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TP</title>
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
+
 <body>
-    
 
+    <nav class="flex">
+        <ul class="flex">
+            <?php foreach ($contenu as $key) {?>
 
+                <li><a href="index.php?page=<?= $key['id']; ?>"><?= $key->menu ?></a></li>
+                
+            <?php } ?>
+        </ul>
+    </nav>
+
+              
+            <?= $contenu->page[intval($numpage)]->title ;?>
+            <?= $contenu->page[intval($numpage)]->content ;?>
+        
 </body>
+
 </html>
